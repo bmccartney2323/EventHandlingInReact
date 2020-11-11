e5569ee94ac69664e4d13ad0a784d8ac6db32078
bmccartney2323 <65781099+bmccartney2323@users.noreply.github.com>
2020-11-11 15:33:00 -0500

    Finished 452 Event Handling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function App() {
+
+const [headingText,setHeadingText] =useState("Hello");
+const [isMouseOver, setMouseOver] = useState( false );
+
+   function mouseOver(){
+     setMouseOver(true);
+   }
+  function mouseOut(){
+     setMouseOver(false);
+  }
+  function handleClick(){
+    setHeadingText("Submitted");
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button style={{backgroundColor:isMouseOver ? "black":"white"}} 
+      onMouseOver={mouseOver}
+      onMouseOut={mouseOut}
+      onClick={handleClick}>Submit</button>
     </div>
   );
 }
